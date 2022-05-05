85e968cc4b7a9a936c09bca881beb40dbf2c280a
jphillips3275 <77686103+jphillips3275@users.noreply.github.com>
2022-05-05 01:33:58 -0500

    z

--- AI.py
+++ AI.py
@@ -2,7 +2,9 @@ import pyautogui
 import random
 import time
 from pynput.keyboard import Controller
-difficulty = 1 #0 = easy, 1 = normal, 2 = hard
+chanceMutation = 5
+chanceUpgrade = 5
+difficulty = 2 #0 = easy, 1 = normal, 2 = hard
 if difficulty == 0:
     from towerPrice import towerPrice
 elif difficulty == 1:
@@ -12,6 +14,7 @@ else:
 
 pyautogui.FAILSAFE = False
 keyboard = Controller()
+instructionSize = 150
 
 def randomCoord():
     xCoord = random.randrange(leftBorder, rightBorder)
@@ -20,12 +23,12 @@ def randomCoord():
 
 def randomTowers(monkeys):
     towers = ["dartMonkey", "boomerangMonkey", "bombShooter", "tackShooter", "iceMonkey", "glueGunner", "sniperMonkey", "monkeyAce",
-    "heliPilot", "mortarMonkey", "wizardMonkey", "superMonkey", "ninjaMonkey", "alchemist", "druid", "spikeFactory", "engineerMonkey"]
+    "heliPilot", "mortarMonkey", "dartlingGunner", "wizardMonkey", "superMonkey", "ninjaMonkey", "alchemist", "druid", "spikeFactory", "engineerMonkey"]
     towersLimited = ["dartMonkey", "boomerangMonkey", "bombShooter", "tackShooter", "sniperMonkey", "wizardMonkey", "ninjaMonkey",
      "alchemist", "druid", "engineerMonkey"]
 
     i=1
-    while i < 100:  #this is stupid but for some reason doing 2 loops isn't working for me and I hate it
+    while i < instructionSize:  #this is stupid but for some reason doing 2 loops isn't working for me and I hate it
         x = random.randrange(0, len(towers))
         monkeys[0].append(towers[x])
         x = random.randrange(0, len(towers))
@@ -126,7 +129,7 @@ def getUpgradeCost(path, targetIndex, monkeysList, coordsList):
     print("buy a", path, "upgrade on a", target, "at location", location, "for price", cost)
     return cost
 
-def buyUpgrade(path, targetIndex, monkeysList, coordsList):
+def buyUpgrade(path, targetIndex, coordsList):
     location = coordsList[targetIndex]
     pyautogui.click(location)
     x = 0
@@ -169,8 +172,7 @@ def getMoneyOCR():
     money = 0
     return money
 
-def playGame(monkeys, coords):
-    income = [650, 121, 137, 138, 175, 164, 163, 182, 200, 199, 314, 189, 192, 282, 259, 266, 268, 165, 358, 260, 186, 351, 298, 277, 167, 335, 333, 662, 266, 389, 337, 537, 627, 205, 912, 1150, 896, 1339, 1277, 1759, 521, 2181, 659, 1278, 1294, 2422, 716, 1637, 2843, 4758, 3016, 1091.5, 1595.5, 1595.5, 924.5, 2197.5, 2483, 1286.5, 1859, 2298, 2159, 922.5, 1232, 1386.4, 2826, 849.8, 3071.6, 1004.2, 1023.6, 777.8, 1391, 2618.8, 1503, 1504, 1392.6, 3044, 2667.4, 1316, 2540.2, 4862, 6709, 1400.2, 5366, 4757, 4749, 7044, 2625.4, 948.5, 2627.4, 3314, 2171, 339.3, 4191, 4537.4, 1946.6, 7667.1, 3718, 9955.6, 1417.2, 9653.8, 2827.9, 1534.6]
+def playGame(monkeys, coords, income):
     money = income[0]
     rounds = 0
 
@@ -185,7 +187,10 @@ def playGame(monkeys, coords):
 
     coordPlace = 1
     while rounds < 100:
+        time.sleep(.5)
         checkWin()
+        if checkDeath() == True:
+            return rounds
 
         #I think we just need to do the OCR stuff once per round, first round doesn't matter since
         #starting income is set and the functions we already have do it accurately, so we should just
@@ -202,22 +207,28 @@ def playGame(monkeys, coords):
             try:
                 if money-getUpgradeCost(monkeys[coordPlace], coords[coordPlace], monkeys, coords) > 0:
                     print("buying upgrade")
-                    buyUpgrade(monkeys[coordPlace], coords[coordPlace], monkeys, coords)
+                    money = money - getUpgradeCost(monkeys[coordPlace], coords[coordPlace], monkeys, coords)
+                    buyUpgrade(monkeys[coordPlace], coords[coordPlace], coords)
                     coordPlace+=1
                 else:
-                    print("upgrade too expensive")
-            except:
+                    print("upgrade too expensive, current money is:",money)
+            except Exception as e:
                 print("upgrade failed, upgrade attempted to be placed on an upgrade instruction")
-                while len(monkeys[coords[coordPlace]]) == 3:
-                    print(monkeys[coords[coordPlace]], coords[coordPlace])
-                    print(len(monkeys[coordPlace]))
-                    coords[coordPlace] = coords[coordPlace]-1
-                if money-getUpgradeCost(monkeys[coordPlace], coords[coordPlace], monkeys, coords) > 0:
-                    print("buying upgrade")
-                    buyUpgrade(monkeys[coordPlace], coords[coordPlace], monkeys, coords)
-                    coordPlace+=1
-                else:
-                    print("upgrade too expensive")
+                print(e)
+                coordPlace+=1
+                # while len(monkeys[coords[coordPlace]]) == 3:
+                #     print(monkeys[coords[coordPlace]], coords[coordPlace])
+                #     print(len(monkeys[coordPlace]))
+                #     coords[coordPlace] = coords[coordPlace]-1
+                # if money-getUpgradeCost(monkeys[coordPlace], coords[coordPlace], monkeys, coords) > 0:
+                #     print("buying upgrade")
+                #     money = money - getUpgradeCost(monkeys[coordPlace], coords[coordPlace], monkeys, coords)
+                #     buyUpgrade(monkeys[coordPlace], coords[coordPlace], coords)
+                #     coordPlace+=1
+                # else:
+                #     print("upgrade too expensive, current money is:",money)
+        if checkDeath() == True:
+            return rounds
         startRound(True)
         rounds+=1
         #can remove this once OCR is implemented?
@@ -234,6 +245,7 @@ def createChildren(monkeys, coords, score):
     sorted.append(score[2])
     sorted.append(score[3])
     sorted.append(score[4])
+    sorted.append(score[5])
     sorted.sort(reverse=True)
     parent1 = score.index(sorted[0])  #parent 1 and 2 should contain the indexes of the top 2 performing monkey arrays
     parent2 = score.index(sorted[1])
@@ -280,38 +292,47 @@ def createChildren(monkeys, coords, score):
             y+=1
         if y == 4:
             y = 0
-        children[4].append(monkeys[parent1][x])
-        cCoords[4].append(coords[parent1][x])
+        children[4] = monkeys[parent1]
+        cCoords[4] = coords[parent1]
 
-        towers = ["dartMonkey", "boomerangMonkey", "bombShooter", "tackShooter", "iceMonkey", "glueGunner", "sniperMonkey", "monkeyAce",
-    "heliPilot", "mortarMonkey", "wizardMonkey", "superMonkey", "ninjaMonkey", "alchemist", "druid", "spikeFactory", "engineerMonkey"]
-        children[5].append(towers[random.randrange(len(towers))])
-        cCoords[5].append(randomCoord())
+    # fully randomized child: good for early game, sucks for late game. Scrapped
+    #     towers = ["dartMonkey", "boomerangMonkey", "bombShooter", "tackShooter", "iceMonkey", "glueGunner", "sniperMonkey", "monkeyAce",
+    # "heliPilot", "mortarMonkey","dartlingGunner", "wizardMonkey", "superMonkey", "ninjaMonkey", "alchemist", "druid", "spikeFactory", "engineerMonkey"]
+    #     towersLimited = ["dartMonkey", "boomerangMonkey", "bombShooter", "tackShooter", "sniperMonkey", "wizardMonkey", "ninjaMonkey",
+    #  "alchemist", "druid", "engineerMonkey"]
+    #     children[5].append(towers[random.randrange(len(towers))])
+    #     children[5][0] = towersLimited[random.randrange(len(towersLimited))]
+    #     cCoords[5].append(randomCoord())
+
+    # this is the same as the highest scoring parent, but it will be mutated to try for improvements
+        children[5] = children[4]
+        cCoords[5] = cCoords[4]
         x+=1
-
     i = 0
     while i < len(monkeys):
         children[i], cCoords[i] = mutate(children[i], cCoords[i])
         i+=1
+    children[4] = monkeys[parent1]
+    cCoords[4] = coords[parent1]
     return children, cCoords
 
 def mutate(children, cCoords):
     #I coded the random towers in a stupid way and I don't want to change it now so this is what we've got
     towers = ["dartMonkey", "boomerangMonkey", "bombShooter", "tackShooter", "iceMonkey", "glueGunner", "sniperMonkey", "monkeyAce",
-    "heliPilot", "mortarMonkey", "wizardMonkey", "superMonkey", "ninjaMonkey", "alchemist", "druid", "spikeFactory", "engineerMonkey"]
+    "heliPilot", "mortarMonkey", "dartlingGunner", "wizardMonkey", "superMonkey", "ninjaMonkey", "alchemist", "druid", "spikeFactory", "engineerMonkey"]
     newTowers = []
     newCoords = []
     i = 0
-    while i < 100:
+    while i < instructionSize:
         newTowers.append(towers[random.randrange(len(towers))])
         newCoords.append(randomCoord())
         i+=1
     newTowers, newCoords = addUpgrades(newTowers, newCoords)
 
-    i = 0
+    i = 1
     while i < len(children):
         rng = random.randrange(10)
-        if rng > 7: #change this value to effect how often mutations happen
+        if rng > chanceMutation: #change this value to effect how often mutations happen
             children[i] = newTowers[i]
             cCoords[i] = newCoords[i]
         i+=1
@@ -322,17 +343,31 @@ def generateUpgradePath():
     digit2 = 0
     digit3 = 0
     x = random.randrange(97)
-    if x <= 20:
+    # if x <= 20:
+    #     digit1 = 0
+    # elif 20 < x <= 50:
+    #     digit1 = 1
+    # elif 50 < x <= 80:
+    #     digit1 = 2
+    # elif 80 < x <= 90:
+    #     digit1 = 3
+    # elif 90 < x <= 97:
+    #     digit1 = 4
+    # elif x > 97:
+    #     digit1 = 5
+
+    #evenly distributed
+    if x <= 17:
         digit1 = 0
-    elif 20 < x <= 50:
+    elif 17 < x <= 34:
         digit1 = 1
-    elif 50 < x <= 80:
+    elif 34 < x <= 51:
         digit1 = 2
-    elif 80 < x <= 90:
+    elif 51 < x <= 68:
         digit1 = 3
-    elif 90 < x <= 97:
+    elif 68 < x <= 85:
         digit1 = 4
-    elif x > 97:
+    elif x > 85:
         digit1 = 5
     if digit1 > 2:
         x = random.randrange(2)
@@ -357,32 +392,62 @@ def generateUpgradePath():
         x = random.randrange(2)
         y = random.randrange(97)
         if x == 0:
-            if y <= 20:
+            # if y <= 20:
+            #     digit2 = 0
+            # elif 20 < y <= 50:
+            #     digit2 = 1
+            # elif 50 < y <= 80:
+            #     digit2 = 2
+            # elif 80 < y <= 90:
+            #     digit2 = 3
+            # elif 90 < y <= 97:
+            #     digit2 = 4
+            # elif y > 97:
+            #     digit2 = 5
+
+            #evenly distributed
+            if x <= 17:
                 digit2 = 0
-            elif 20 < y <= 50:
+            elif 17 < x <= 34:
                 digit2 = 1
-            elif 50 < y <= 80:
+            elif 34 < x <= 51:
                 digit2 = 2
-            elif 80 < y <= 90:
+            elif 51 < x <= 68:
                 digit2 = 3
-            elif 90 < y <= 97:
+            elif 68 < x <= 85:
                 digit2 = 4
-            elif y > 97:
+            elif x > 85:
                 digit2 = 5
+
             digit3 = 0
         else:
-            if y <= 20:
+            # if y <= 20:
+            #     digit3 = 0
+            # elif 20 < y <= 50:
+            #     digit3 = 1
+            # elif 50 < y <= 80:
+            #     digit3 = 2
+            # elif 80 < y <= 90:
+            #     digit3 = 3
+            # elif 90 < y <= 97:
+            #     digit3 = 4
+            # elif y > 97:
+            #     digit3 = 5
+
+            #evenly distributed
+            if x <= 17:
                 digit3 = 0
-            elif 20 < y <= 50:
+            elif 17 < x <= 34:
                 digit3 = 1
-            elif 50 < y <= 80:
+            elif 34 < x <= 51:
                 digit3 = 2
-            elif 80 < y <= 90:
+            elif 51 < x <= 68:
                 digit3 = 3
-            elif 90 < y <= 97:
+            elif 68 < x < 85:
                 digit3 = 4
-            elif y > 97:
+            elif x > 85:
                 digit3 = 5
+
             digit2 = 0
     if digit1 == 0 and digit2 == 0 and digit3 == 0:
         return generateUpgradePath()
@@ -391,15 +456,19 @@ def generateUpgradePath():
     
 
 def addUpgrades(monkeys, coords):
-    x = 5
+    x = 3 #number of towers bought before we start generating upgrades
     usedCoords = []
     while x < len(monkeys):
         r = random.randrange(10)
-        if r > 5: #change this to change the rate of upgrades
+        if r > chanceUpgrade: #change this to change the rate of upgrades
             monkeys[x] = generateUpgradePath()
             upgradeTarget = random.randrange(x)
-            while upgradeTarget in usedCoords:
+            emergencyBreak = 0
+            while (upgradeTarget in usedCoords) or len(monkeys[upgradeTarget]) == 3:
                 upgradeTarget = random.randrange(x)
+                if emergencyBreak == instructionSize:
+                    break
+                emergencyBreak+=1
             usedCoords.append(upgradeTarget)
             coords[x] = upgradeTarget
         x+=1
@@ -437,7 +506,7 @@ score = [0, 0, 0, 0, 0, 0]
 
 monkeys = randomTowers(monkeys)
 i=0
-while i < 100:
+while i < instructionSize:
     coords[0].append(randomCoord())
     coords[1].append(randomCoord())
     coords[2].append(randomCoord())
@@ -446,24 +515,47 @@ while i < 100:
     coords[5].append(randomCoord())
     i+=1
 i = 0
+income = [650, 121, 137, 138, 175, 164, 163, 182, 200, 199, 314, 189, 192, 282, 259, 266, 268, 165, 358, 260, 186, 351, 298, 277, 167, 335, 333, 662, 266, 389, 337, 537, 627, 205, 912, 1150, 896, 1339, 1277, 1759, 521, 2181, 659, 1278, 1294, 2422, 716, 1637, 2843, 4758, 3016, 1091.5, 1595.5, 1595.5, 924.5, 2197.5, 2483, 1286.5, 1859, 2298, 2159, 922.5, 1232, 1386.4, 2826, 849.8, 3071.6, 1004.2, 1023.6, 777.8, 1391, 2618.8, 1503, 1504, 1392.6, 3044, 2667.4, 1316, 2540.2, 4862, 6709, 1400.2, 5366, 4757, 4749, 7044, 2625.4, 948.5, 2627.4, 3314, 2171, 339.3, 4191, 4537.4, 1946.6, 7667.1, 3718, 9955.6, 1417.2, 9653.8, 2827.9, 1534.6]
+incomeHard = [650, 138, 175, 164, 163, 182, 200, 199, 314, 189, 192, 282, 259, 266, 268, 165, 358, 260, 186, 351, 298, 277, 167, 335, 333, 662, 266, 389, 337, 537, 627, 205, 912, 1150, 896, 1339, 1277, 1759, 521, 2181, 659, 1278, 1294, 2422, 716, 1637, 2843, 4758, 3016, 1091.5, 1595.5, 1595.5, 924.5, 2197.5, 2483, 1286.5, 1859, 2298, 2159, 922.5, 1232, 1386.4, 2826, 849.8, 3071.6, 1004.2, 1023.6, 777.8, 1391, 2618.8, 1503, 1504, 1392.6, 3044, 2667.4, 1316, 2540.2, 4862, 6709, 1400.2, 5366, 4757, 4749, 7044, 2625.4, 948.5, 2627.4, 3314, 2171, 339.3, 4191, 4537.4, 1946.6, 7667.1, 3718, 9955.6, 1417.2, 9653.8, 2827.9, 1534.6]
 while i < 6:
     monkeys[i], coords[i] = addUpgrades(monkeys[i], coords[i])
     i+=1
 a = 0
-while a < 5:
+while a < 50:
     print(monkeys[0][a], coords[0][a])
     a+=1
 #uncomment sleep for single screen computers
 #time.sleep(5)
+
+#score = [17, 26, 34, 8, 13, 37]
+
 pyautogui.click(500,500)
+numTrials = 1
+file = open("scores.txt", "w")
+for scores in score:
+    file.write(str(scores))
+    file.write(" ")
+file.write("\n")
+file.close()
 while 1:
     p = 0
     while p <= 5:
         print("new testing round:",p)
-        score[p] = playGame(monkeys[p], coords[p])
+        print("number of trials:", numTrials)
+        if difficulty == 2:
+            score[p] = playGame(monkeys[p], coords[p], incomeHard)
+        else:
+            score[p] = playGame(monkeys[p], coords[p], income)
         pyautogui.click(853,817)
         time.sleep(.3)
         pyautogui.click(1146, 728)
         p+=1
+        numTrials+=1
     print(score)
+    file = open("scores.txt", "a")
+    for scores in score:
+        file.write(str(scores))
+        file.write(" ")
+    file.write("\n")
+    file.close()
     monkeys, coords = createChildren(monkeys, coords, score)
